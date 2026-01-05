31f616aed1ceb618a8f5a43aca1fe9988e468137
Devika Kinnal <devikakinnal@gmail.com>
2026-01-05 23:03:31 +0530

    Create dropdownflow.java
    Added new Selenium automation flow for SpaceJet

---
+++ dropdownflow.java
@@ -0,0 +1,21 @@
+package com.automation.spacejet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.sun.org.apache.bcel.internal.generic.Select;
+
+public class Dropdownflow {
+	public static void main(String[] args) {
+			System.setProperty("webdriver.chrome.driver","/Users/devik/Documents/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.spicejet.com/");
+			WebElement staticDropdown=driver.findElement(By.class("css-1dbjc4n r-1habvwh r-1loqt21 r-1777fci r-1mi0q7o r-1yt7n81 r-m611by r-1otgn73"));
+			org.openqa.selenium.support.ui.Select dropdown =new Select(StaticDropdown);
+			dropdown.selectByIndex(3);
+			
+	}
+}
